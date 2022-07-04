5d82a4ba52a8ed9b3a1cdb755924c106167f2e90
Chantouch <chantouchsek.cs83@gmail.com>
2022-07-04 22:14:57 +0700

    perf: optimized min 30%

--- __tests__/min.spec.ts
+++ __tests__/min.spec.ts
@@ -7,50 +7,22 @@ describe('min validation rule', () => {
   })
 
   it('should pass with the name "Da". Minimum is 2 letters.', () => {
-    const validator = new Validator(
-      {
-        name: 'Da',
-      },
-      {
-        name: 'min:2',
-      },
-    )
+    const validator = new Validator({ name: 'Da' }, { name: 'min:2' })
     expect(validator.passes()).toBeTruthy()
   })
 
   it('should pass with the age "18". Minimum is 18.', () => {
-    const validator = new Validator(
-      {
-        age: 18,
-      },
-      {
-        age: 'min:18',
-      },
-    )
+    const validator = new Validator({ age: 18 }, { age: 'min:18' })
     expect(validator.passes()).toBeTruthy()
   })
 
   it('should fail with the age "17". Minimum is 18.', () => {
-    const validator = new Validator(
-      {
-        age: 17,
-      },
-      {
-        age: 'min:18',
-      },
-    )
+    const validator = new Validator({ age: 17 }, { age: 'min:18' })
     expect(validator.fails()).toBeTruthy()
   })
 
   it('should fail with value of 0.04', () => {
-    const validator = new Validator(
-      {
-        val: 0.04,
-      },
-      {
-        val: 'min:0.05',
-      },
-    )
+    const validator = new Validator({ val: 0.04 }, { val: 'min:0.05' })
     expect(validator.fails()).toBeTruthy()
   })
 
