25152f9569f1d58cf4f13d1e460f13e13217080f
Chantouch <chantouchsek.cs83@gmail.com>
2022-12-16 20:04:02 +0700

    feat: :tada: re-adjust the vite.config

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { resolve } from 'path'
+import typescript from '@rollup/plugin-typescript'
+import { typescriptPaths } from 'rollup-plugin-typescript-paths'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  plugins: [],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: 'dist',
+        }),
+      ],
+    },
+  },
+})
