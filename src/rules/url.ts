6dd97ee2035dce79d63924d2fe8c9351bc5f5d86
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2023-01-28 17:37:02 +0600

    handle localhost urls validation

--- src/rules/url.ts
+++ src/rules/url.ts
@@ -1,3 +1,8 @@
 export const url = (value: string) => {
-  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,63}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/i.test(value)
+  try {
+    new URL(value)
+    return true
+  } catch (error) {
+    return false
+  }
 }
